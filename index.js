f8ec35437ed97da1f4268df44f1bb24c2db54fef
Anurag Mishra <anurag026mishra@gmail.com>
2024-12-05 00:37:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-let port = process.env.PORT || 3000;
-
-let IO = require("socket.io")(port, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
 });
+const { v4: uuidV4 } = require('uuid')
 
-IO.use((socket, next) => {
-  if (socket.handshake.query) {
-    let callerId = socket.handshake.query.callerId;
-    socket.user = callerId;
-    next();
-  }
-});
+app.use('/peerjs', peerServer);
 
-IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
-  socket.join(socket.user);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-  socket.on("makeCall", (data) => {
-    let calleeId = data.calleeId;
-    let sdpOffer = data.sdpOffer;
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
-    socket.to(calleeId).emit("newCall", {
-      callerId: socket.user,
-      sdpOffer: sdpOffer,
-    });
-  });
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
-  socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
 
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
-    });
-  });
-
-  socket.on("IceCandidate", (data) => {
-    let calleeId = data.calleeId;
-    let iceCandidate = data.iceCandidate;
-
-    socket.to(calleeId).emit("IceCandidate", {
-      sender: socket.user,
-      iceCandidate: iceCandidate,
-    });
-  });
-});
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
